0a279718e883f4266956a7eb7a1975881a792a89
Rory Chapman <roch2704@colorado.edu>
2021-05-10 10:11:39 -0600

    Updated get_system_info to strip null characters from strings, this is for Windows support.  cargo clippy, cargo fmt.

--- system-stats/src/lib.rs
+++ system-stats/src/lib.rs
@@ -14,13 +14,13 @@
 * along with PWshSystemStats.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-extern crate sysinfo;
 extern crate libc;
+extern crate sysinfo;
 
-use std::ffi::{CString};
 use libc::c_char;
+use std::ffi::CString;
 
-use sysinfo::{System, SystemExt, ProcessorExt, RefreshKind, DiskExt};
+use sysinfo::{DiskExt, ProcessorExt, RefreshKind, System, SystemExt};
 //use sysinfo::{ ProcessExt };
 use std::thread::sleep;
 
@@ -45,7 +45,6 @@ pub struct DiskInfo {
     pub free_space: Vec<u64>,
     pub total_space: Vec<u64>,
     pub length: i32,
-
 }
 
 impl DiskInfo {
@@ -58,7 +57,6 @@ impl DiskInfo {
     }
 
     fn query_disk_usage(&mut self) {
-
         let sys = System::new_all();
         for disk in sys.get_disks() {
             let free_space = disk.get_available_space();
@@ -66,17 +64,17 @@ impl DiskInfo {
             self.free_space.push(free_space);
             self.total_space.push(total_space);
             self.length += 1;
-        };
+        }
     }
-    
-    fn get_disk_usage_freespace(&self, index: usize) -> u64{
-        self.free_space[index]        
+
+    fn get_disk_usage_freespace(&self, index: usize) -> u64 {
+        self.free_space[index]
     }
 
     fn get_disk_usage_totalspace(&self, index: usize) -> u64 {
         self.total_space[index]
     }
-    
+
     fn get_disk_usage_diskcount(&self) -> i32 {
         self.length
     }
@@ -85,7 +83,7 @@ impl DiskInfo {
 #[no_mangle]
 pub extern "C" fn get_used_memory_info() -> u64 {
     let sys = System::new_all();
-    
+
     sys.get_used_memory()
 }
 
@@ -97,28 +95,25 @@ pub extern "C" fn get_total_memory_info() -> u64 {
 
 #[no_mangle]
 pub extern "C" fn get_system_info() -> *mut c_char {
-
     let sys = System::new_all();
-    let mut results = vec!();
-    
+    let mut results = vec![];
+
     match sys.get_host_name() {
-        Some(h) => results.push(h),
-        _ => results.push("unknown hostname".to_string())
+        Some(h) => results.push(h.trim_matches(char::from(0)).to_string()),
+        _ => results.push("unknown hostname".to_string()),
     }
     match sys.get_name() {
-        Some(n) => results.push(n),
-        _ => results.push("unknown".to_string())
+        Some(n) => results.push(n.trim_matches(char::from(0)).to_string()),
+        _ => results.push("unknown".to_string()),
     };
 
     match sys.get_os_version() {
-        Some(v) => results.push(v),
-        _ => results.push("version unknown".to_string())
+        Some(v) => results.push(v.trim_matches(char::from(0)).to_string()),
+        _ => results.push("version unknown".to_string()),
     };
-    
 
     let out = results.join(" - ");
     CString::new(out).unwrap().into_raw()
-    
 }
 
 /// # Safety
@@ -127,7 +122,6 @@ pub extern "C" fn get_system_info() -> *mut c_char {
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
 pub unsafe extern "C" fn free_system_info_results(s: *mut c_char) {
-
     // unsafe
     if s.is_null() {
         return;
@@ -141,13 +135,11 @@ pub extern "C" fn get_processor_usage() -> f64 {
     sys.refresh_cpu();
     sleep(std::time::Duration::from_millis(500));
     sys.refresh_cpu();
-    let mut usage = vec!();
+    let mut usage = vec![];
     for p in sys.get_processors() {
         usage.push(p.get_cpu_usage() as f64)
     }
-    let total = usage.iter().fold(0.0, |u,v| {
-        u + v
-    });
+    let total = usage.iter().fold(0.0, |u, v| u + v);
     total / (sys.get_processors().len() as f64)
 }
 
@@ -157,7 +149,7 @@ pub extern "C" fn get_core_usage() -> *mut c_char {
     sleep(std::time::Duration::from_millis(500));
     sys.refresh_cpu();
 
-    let mut usage = vec!();
+    let mut usage = vec![];
     for p in sys.get_processors() {
         //println!("CPU usage {:?}", p.get_cpu_usage());
         usage.push(p.get_cpu_usage().to_string())
@@ -165,7 +157,7 @@ pub extern "C" fn get_core_usage() -> *mut c_char {
 
     let out = usage.join(" - ");
     CString::new(out).unwrap().into_raw()
-} 
+}
 
 /// # Safety
 ///
@@ -173,7 +165,6 @@ pub extern "C" fn get_core_usage() -> *mut c_char {
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
 pub unsafe extern "C" fn free_core_usage(s: *mut c_char) {
-
     // unsafe
     if s.is_null() {
         return;
@@ -186,15 +177,15 @@ pub extern "C" fn get_load_avg() -> *mut LoadAvg {
     let mut sys = System::new_with_specifics(RefreshKind::new().with_cpu());
     sys.refresh_all();
     sys.refresh_all();
-    let load = sys.get_load_average(); 
-    
+    let load = sys.get_load_average();
+
     let load_avg = LoadAvg {
         one: load.one,
         five: load.five,
-        fifteen: load.fifteen
+        fifteen: load.fifteen,
     };
-    
-    Box::into_raw(Box::new(load_avg))  
+
+    Box::into_raw(Box::new(load_avg))
 }
 
 /// # Safety
@@ -203,14 +194,12 @@ pub extern "C" fn get_load_avg() -> *mut LoadAvg {
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
 pub unsafe extern "C" fn free_load_avg(s: *mut LoadAvg) {
-
     if s.is_null() {
         return;
     }
-    
+
     // unsafe
     Box::from_raw(s);
-        
 }
 
 #[no_mangle]
@@ -221,7 +210,6 @@ pub extern "C" fn get_free_memory() -> u64 {
 
 #[no_mangle]
 pub extern "C" fn disk_info_new() -> *mut DiskInfo {
-
     Box::into_raw(Box::new(DiskInfo::new()))
 }
 
@@ -237,7 +225,6 @@ pub unsafe extern "C" fn free_disk_info(disk_info: *mut DiskInfo) {
 
     // unsafe
     Box::from_raw(disk_info);
-
 }
 
 /// # Safety
@@ -246,10 +233,9 @@ pub unsafe extern "C" fn free_disk_info(disk_info: *mut DiskInfo) {
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
 pub unsafe extern "C" fn disk_info_query(disk_info_ptr: *mut DiskInfo) {
-    
     // unsafe
     let disk_info = {
-        assert!(! disk_info_ptr.is_null());  //panic if null
+        assert!(!disk_info_ptr.is_null()); //panic if null
         &mut *disk_info_ptr
     };
     disk_info.query_disk_usage();
@@ -260,15 +246,16 @@ pub unsafe extern "C" fn disk_info_query(disk_info_ptr: *mut DiskInfo) {
 /// This function should not be called if the pointer passed
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
-pub unsafe extern "C" fn disk_info_get_disk_freespace(disk_info_ptr: *mut DiskInfo,
-                                                     index: usize) -> u64 {
-
+pub unsafe extern "C" fn disk_info_get_disk_freespace(
+    disk_info_ptr: *mut DiskInfo,
+    index: usize,
+) -> u64 {
     // unsafe
     let disk_info = {
-        assert!(! disk_info_ptr.is_null());  //panic if null
+        assert!(!disk_info_ptr.is_null()); //panic if null
         &*disk_info_ptr
     };
-    disk_info.get_disk_usage_freespace(index)    
+    disk_info.get_disk_usage_freespace(index)
 }
 
 /// # Safety
@@ -276,12 +263,13 @@ pub unsafe extern "C" fn disk_info_get_disk_freespace(disk_info_ptr: *mut DiskIn
 /// This function should not be called if the pointer passed
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
-pub unsafe extern "C" fn disk_info_get_disk_totalspace(disk_info_ptr: *mut DiskInfo,
-                                                index: usize) -> u64 {
-
+pub unsafe extern "C" fn disk_info_get_disk_totalspace(
+    disk_info_ptr: *mut DiskInfo,
+    index: usize,
+) -> u64 {
     // unsafe
     let disk_info = {
-        assert!(! disk_info_ptr.is_null());  // panic if pointer is null
+        assert!(!disk_info_ptr.is_null()); // panic if pointer is null
         &*disk_info_ptr
     };
 
@@ -293,12 +281,10 @@ pub unsafe extern "C" fn disk_info_get_disk_totalspace(disk_info_ptr: *mut DiskI
 /// This function should not be called if the pointer passed
 /// in is null or invalid.  Pass in a valid pointer to free it exactly once.
 #[no_mangle]
-pub unsafe extern "C" fn disk_info_get_disk_usage_diskcount(disk_info_ptr: *mut DiskInfo)
-                                                     -> i32 {
-    
+pub unsafe extern "C" fn disk_info_get_disk_usage_diskcount(disk_info_ptr: *mut DiskInfo) -> i32 {
     // unsafe
     let disk_info = {
-        assert!(! disk_info_ptr.is_null());  //panic if null
+        assert!(!disk_info_ptr.is_null()); //panic if null
         &*disk_info_ptr
     };
     disk_info.get_disk_usage_diskcount()
@@ -329,41 +315,38 @@ mod tests {
         assert_eq!((total_memory >= 1), true);
 
         let free_memory = get_free_memory();
-        assert_eq!((free_memory >= 1),true);
+        assert_eq!((free_memory >= 1), true);
     }
-    
+
     #[test]
     fn check_reference_types() {
         let core_usage = get_core_usage();
-        assert_ne!(core_usage.is_null(),true);
+        assert_ne!(core_usage.is_null(), true);
         unsafe {
             free_core_usage(core_usage);
         }
         let system_info = get_system_info();
         assert_ne!(system_info.is_null(), true);
-        
+
         unsafe {
             free_system_info_results(system_info);
         }
 
         let load_average = get_load_avg();
         assert_ne!(load_average.is_null(), true);
-        
+
         unsafe {
             free_load_avg(load_average as *mut LoadAvg);
         }
-        
+
         let mut disk_info = DiskInfo::new();
         disk_info.query_disk_usage();
 
-        assert_eq!((disk_info.length > 0),true);
+        assert_eq!((disk_info.length > 0), true);
 
         assert_eq!((disk_info.free_space[0] > 0), true);
         assert_eq!((disk_info.total_space[0] > 0), true);
         //free_disk_info(disk_info as *mut DiskInfo);
         //free_disk_info(disk_info);
-
-
     }
-
 }
